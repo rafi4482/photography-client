a552d1cf1efe474b60366b94fe24237f8d6c7bd9
Md Rafi Uz Zaman <rafi.kazla@gmail.com>
2023-07-27 08:19:00 +0600

    routing

--- src/components/Instructors/Instructors.jsx
+++ src/components/Instructors/Instructors.jsx
@@ -9,19 +9,68 @@ import {
   Icon,
   chakra,
   Tooltip,
+  SimpleGrid, // Add SimpleGrid from Chakra UI
 } from "@chakra-ui/react";
 import { BsStar, BsStarFill, BsStarHalf } from "react-icons/bs";
 import { FiShoppingCart } from "react-icons/fi";
 
-const data = {
-  isNew: true,
-  imageURL:
-    "https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=4600&q=80",
-  name: "Wayfarer Classic",
-  price: 4.5,
-  rating: 4.2,
-  numReviews: 34,
-};
+const data = [
+  // Create an array of card data, you can add more objects here to create more cards
+  {
+    isNew: true,
+    imageURL:
+      "https://images.pexels.com/photos/7752812/pexels-photo-7752812.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    name: "Wayfarer Classic",
+    price: 4.5,
+    rating: 4.2,
+    numReviews: 34,
+  },
+  {
+    isNew: true,
+    imageURL:
+      "https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=4600&q=80",
+    name: "Wayfarer Classic",
+    price: 4.5,
+    rating: 4.2,
+    numReviews: 34,
+  },
+  {
+    isNew: true,
+    imageURL:
+      "https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=4600&q=80",
+    name: "Wayfarer Classic",
+    price: 4.5,
+    rating: 4.2,
+    numReviews: 34,
+  },
+  {
+    isNew: true,
+    imageURL:
+      "https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=4600&q=80",
+    name: "Wayfarer Classic",
+    price: 4.5,
+    rating: 4.2,
+    numReviews: 34,
+  },
+  {
+    isNew: true,
+    imageURL:
+      "https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=4600&q=80",
+    name: "Wayfarer Classic",
+    price: 4.5,
+    rating: 4.2,
+    numReviews: 34,
+  },
+  {
+    isNew: true,
+    imageURL:
+      "https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=4600&q=80",
+    name: "Wayfarer Classic",
+    price: 4.5,
+    rating: 4.2,
+    numReviews: 34,
+  },
+];
 
 function Rating({ rating, numReviews }) {
   return (
@@ -53,73 +102,102 @@ function Rating({ rating, numReviews }) {
 
 function Instructors() {
   return (
-    <Flex p={50} w="full" alignItems="center" justifyContent="center">
-      <Box
-        bg={useColorModeValue("white", "gray.800")}
-        maxW="sm"
-        borderWidth="1px"
-        rounded="lg"
-        shadow="lg"
-        position="relative"
+    <Flex
+      p={50}
+      w="full"
+      alignItems="center"
+      justifyContent="center"
+      minH="100vh"
+      bg={useColorModeValue("gray.50", "gray.800")}
+    >
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 3 }} // Display 1 card on small, 2 on medium, and 3 on large and extra-large screens
+        spacing={8} // Adjust spacing between cards as needed
+        width="full"
       >
-        {data.isNew && (
-          <Circle
-            size="10px"
-            position="absolute"
-            top={2}
-            right={2}
-            bg="red.200"
-          />
-        )}
-
-        <Image
-          src={data.imageURL}
-          alt={`Picture of ${data.name}`}
-          roundedTop="lg"
-        />
-
-        <Box p="6">
-          <Box display="flex" alignItems="baseline">
-            {data.isNew && (
-              <Badge rounded="full" px="2" fontSize="0.8em" colorScheme="red">
-                New
-              </Badge>
+        {data.map((item, index) => (
+          <Box
+            key={index}
+            bg={useColorModeValue("white", "gray.800")}
+            borderWidth="1px"
+            rounded="lg"
+            shadow="lg"
+            position="relative"
+            overflow="hidden"
+          >
+            {item.isNew && (
+              <Circle
+                size="10px"
+                position="absolute"
+                top={2}
+                right={2}
+                bg="red.200"
+              />
             )}
-          </Box>
-          <Flex mt="1" justifyContent="space-between" alignContent="center">
-            <Box
-              fontSize="2xl"
-              fontWeight="semibold"
-              as="h4"
-              lineHeight="tight"
-              isTruncated
-            >
-              {data.name}
-            </Box>
-            <Tooltip
-              label="Add to cart"
-              bg="white"
-              placement={"top"}
-              color={"gray.800"}
-              fontSize={"1.2em"}
-            >
-              <chakra.a href={"#"} display={"flex"}>
-                <Icon as={FiShoppingCart} h={7} w={7} alignSelf={"center"} />
-              </chakra.a>
-            </Tooltip>
-          </Flex>
 
-          <Flex justifyContent="space-between" alignContent="center">
-            <Rating rating={data.rating} numReviews={data.numReviews} />
-            <Box fontSize="2xl" color={useColorModeValue("gray.800", "white")}>
-              <Box as="span" color={"gray.600"} fontSize="lg">
-                £
+            <Image
+              src={item.imageURL}
+              alt={`Picture of ${item.name}`}
+              roundedTop="lg"
+            />
+
+            <Box p="6">
+              <Box display="flex" alignItems="baseline">
+                {item.isNew && (
+                  <Badge
+                    rounded="full"
+                    px="2"
+                    fontSize="0.8em"
+                    colorScheme="red"
+                  >
+                    New
+                  </Badge>
+                )}
               </Box>
-              {data.price.toFixed(2)}
+              <Flex mt="1" justifyContent="space-between" alignContent="center">
+                <Box
+                  fontSize="2xl"
+                  fontWeight="semibold"
+                  as="h4"
+                  lineHeight="tight"
+                  isTruncated
+                >
+                  {item.name}
+                </Box>
+                <Tooltip
+                  label="Add to cart"
+                  bg="white"
+                  placement={"top"}
+                  color={"gray.800"}
+                  fontSize={"1.2em"}
+                >
+                  <chakra.a href={"#"} display={"flex"}>
+                    <Icon
+                      as={FiShoppingCart}
+                      h={7}
+                      w={7}
+                      alignSelf={"center"}
+                    />
+                  </chakra.a>
+                </Tooltip>
+              </Flex>
+
+              <Flex justifyContent="space-between" alignContent="center">
+                <Rating rating={item.rating} numReviews={item.numReviews} />
+                <Box
+                  fontSize="2xl"
+                  color={useColorModeValue("gray.800", "white")}
+                >
+                  <Box as="span" color={"gray.600"} fontSize="lg">
+                    £
+                  </Box>
+                  {item.price.toFixed(2)}
+                </Box>
+              </Flex>
             </Box>
-          </Flex>
-        </Box>
-      </Box>
+          </Box>
+        ))}
+      </SimpleGrid>
     </Flex>
   );
 }
