9d8a8203cf878a22c147aabeed948fcaec6192a0
Md Rafi Uz Zaman <rafi.kazla@gmail.com>
2023-07-27 08:29:46 +0600

    routing

--- src/components/Instructors/Instructors.jsx
+++ src/components/Instructors/Instructors.jsx
@@ -99,7 +99,6 @@ function Rating({ rating, numReviews }) {
     </Box>
   );
 }
-
 function Instructors() {
   return (
     <Flex
@@ -141,57 +140,38 @@ function Instructors() {
               roundedTop="lg"
             />
 
-            <Box p="6">
-              <Box display="flex" alignItems="baseline">
-                {item.isNew && (
-                  <Badge
-                    rounded="full"
-                    px="2"
-                    fontSize="0.8em"
-                    colorScheme="red"
-                  >
-                    New
-                  </Badge>
-                )}
+            <Box
+              p="2"
+              display="flex"
+              flexDirection="column"
+              alignItems="center"
+              justifyContent="center"
+            >
+              <Box
+                p="8"
+                fontSize="2xl"
+                fontWeight="semibold"
+                as="h4"
+                lineHeight="tight"
+                isTruncated
+                textAlign="center"
+              >
+                {item.name}
               </Box>
-              <Flex mt="1" justifyContent="space-between" alignContent="center">
-                <Box
-                  fontSize="2xl"
-                  fontWeight="semibold"
-                  as="h4"
-                  lineHeight="tight"
-                  isTruncated
-                >
-                  {item.name}
-                </Box>
-                <Tooltip
-                  label="Add to cart"
-                  bg="white"
-                  placement={"top"}
-                  color={"gray.800"}
-                  fontSize={"1.2em"}
-                >
-                  <chakra.a href={"#"} display={"flex"}>
-                    <Icon
-                      as={FiShoppingCart}
-                      h={7}
-                      w={7}
-                      alignSelf={"center"}
-                    />
-                  </chakra.a>
-                </Tooltip>
-              </Flex>
 
-              <Flex justifyContent="space-between" alignContent="center">
+              <Flex
+                justifyContent="space-between"
+                alignContent="center"
+                width="100%"
+              >
                 <Rating rating={item.rating} numReviews={item.numReviews} />
                 <Box
                   fontSize="2xl"
                   color={useColorModeValue("gray.800", "white")}
                 >
                   <Box as="span" color={"gray.600"} fontSize="lg">
-                    £
+                    a@gmail.com
                   </Box>
-                  {item.price.toFixed(2)}
                 </Box>
               </Flex>
             </Box>
@@ -201,5 +181,4 @@ function Instructors() {
     </Flex>
   );
 }
-
 export default Instructors;
